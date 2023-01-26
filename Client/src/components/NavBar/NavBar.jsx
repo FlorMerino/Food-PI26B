011252e1f8206d6ffbb07a6916c3410aa7b09bc9
FlorMerino <florenciamerino012@gmail.com>
2023-01-26 16:49:01 -0300

    modificaciones front

--- Client/src/components/NavBar/NavBar.jsx
+++ Client/src/components/NavBar/NavBar.jsx
@@ -11,7 +11,7 @@ export default function NavBar({setPage}) {
     //     <li ><Link to={'/create'}><button className={styles.createBTN}>Create Recipe</button></Link></li>
     //     <li> <a href="#recipes">Recipes</a> </li>
     //   </ul>
-    <nav class="navbar navbar-expand-lg  styles">
+    <nav class="navbar navbar-expand-lg styles">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
