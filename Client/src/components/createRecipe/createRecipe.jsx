68079d8fb104519c2d9b319cdfc725a0926d132d
FlorMerino <florenciamerino012@gmail.com>
2023-03-27 00:52:27 -0300

    Error e create recipe

--- Client/src/components/createRecipe/createRecipe.jsx
+++ Client/src/components/createRecipe/createRecipe.jsx
@@ -8,21 +8,21 @@ import Select from "react-select";
 
 
 function validate(input) {
-  let score= parseInt(input.healthScore);
-  
-  let error = {    
+  let score = parseInt(input.healthScore);
+
+  let error = {
   };
 
   if (!input.name) error.name = 'Title is required!';
   if (!input.summary) error.summary = 'Please write a summary for your recipe!';
   if (!input.steps) error.steps = 'Please write a steps for your recipe!';
 
-  if ( score === 0) error.healthScore = 'Please enter the healthScore';
-  if(!input.name || input.diets.length === 0 || input.dishTypes.length ===0 || !input.summary || !input.steps || input.healthScore ===0) {
+  if (score === 0) error.healthScore = 'Please enter the healthScore';
+  if (!input.name || input.diets.length === 0 || input.dishTypes.length === 0 || !input.summary || !input.steps || input.healthScore === 0) {
     error.switchS = true
   }
 
-  if(input.name && input.diets.length> 0 && input.dishTypes.length>0 && input.summary && input.steps && input.healthScore >0) {
+  if (input.name && input.diets.length > 0 && input.dishTypes.length > 0 && input.summary && input.steps && input.healthScore > 0) {
     error.switchS = false
   }
   if (input.diets.length === 0 || input.dishTypes.length === 0) error.dietsTypes = 'Select at least one type and one diet!';
@@ -45,7 +45,7 @@ export default function AddRecipe() {
   }, [dispatch])
 
   const [error, setError] = useState({});
-  const [viewImageSelect, setViewImageSelect] = useState()
+  const [viewImageSelect, setViewImageSelect] = useState(undefined)
 
   const [input, setInput] = useState({
     name: '',
@@ -58,15 +58,13 @@ export default function AddRecipe() {
     dishTypes: []
   });
 
-  console.log(input)
-  
-
   let handleChange = (e) => {
-    console.log(e)
+
     if (e.target.name === 'image') {
+      e.preventDefault();
       let url = URL.createObjectURL(e.target.files[0])
       setViewImageSelect(url)
-      console.log(url)
+
     }
     e.preventDefault();
     setInput({
@@ -83,15 +81,15 @@ export default function AddRecipe() {
 
   let handleSelectDiet = (diet) => {
     setInput({
-      ...input, 
-      ['diets']: diet.map(e=> e.value)
+      ...input,
+      ['diets']: diet.map(e => e.value)
     });
   }
 
   let handleSelectType = (type) => {
     setInput({
       ...input,
-      ['dishTypes']: type.map(e=> e.value),
+      ['dishTypes']: type.map(e => e.value),
     });
   }
 
@@ -105,7 +103,7 @@ export default function AddRecipe() {
       steps: '',
       healthScore: '',
       steps: '',
-      img: '',
+      image: undefined,
       diets: [],
       dishTypes: []
     });
@@ -122,18 +120,18 @@ export default function AddRecipe() {
       <div>
         <h2>Create your Recipe! </h2>
       </div>
-      <form class={`was-validated ${styles.containerForm}`} onSubmit={(e) => { handleSubmit(e) }}>
+      <form className={`was-validated ${styles.containerForm}`} onSubmit={(e) => { handleSubmit(e) }}>
         <div className={styles.containerForm2} >
-          <div class={`${styles.subContainer} mb-3`}>
-            <label htmlFor="validationTextarea" class="form-label">Title recipe</label>
-            <input onChange={(e) => handleChange(e)} name='name' type="text" value={input.name} class="form-control" id="validationTextarea" placeholder="Title..." required></input>
-            <div class="invalid-feedback">
-            { error.name && (
-                          <div className={styles.danger}>{error.name}</div>
-                  )}
-              
+          <div className={`${styles.subContainer} mb-3`}>
+            <label htmlFor="validationTextarea" className="form-label">Title recipe</label>
+            <input onChange={(e) => handleChange(e)} name='name' type="text" value={input.name} className="form-control" id="validationTextarea" placeholder="Title..." required></input>
+            <div className="invalid-feedback">
+              {error.name && (
+                <div className={styles.danger}>{error.name}</div>
+              )}
+
             </div>
-            <div>
+            {/* <div>
             <Select isMulti options={listDiets} name='diets' className={`${styles.multiSelect} basic-multi-select`}
               onChange={(diet) => handleSelectDiet(diet)} placeholder='Select diet'></Select>
  
@@ -144,51 +142,51 @@ export default function AddRecipe() {
                           <div className={styles.danger}>{error.dietsTypes}</div>
                   )}
             </div> 
-            </div>
-           
+            </div> */}
+
 
             <div>
-              <label for="customRange1" class="form-label">Health Score: {input.healthScore}</label>
-              <input type="range" defaultValue={0} onChange={(e) => handleChange(e)} class="form-range" id="customRange1" min="0" max="100" step="10" name="healthScore" />
+              <label htmlFor="customRange1" className="form-label">Health Score: {input.healthScore}</label>
+              <input type="range" defaultValue={0} onChange={(e) => handleChange(e)} className="form-range" id="customRange1" min="0" max="100" step="10" name="healthScore" />
               <div >
-                 { error.healthScore && (
-                          <div className={styles.danger}>{error.healthScore}</div>
-                  )}
+                {error.healthScore && (
+                  <div className={styles.danger}>{error.healthScore}</div>
+                )}
               </div>
             </div>
 
             <div>
-            <label>Image url:</label>
-            <input onChange={(e) => handleChange(e)} id="files" name="image" type="file" class="form-control" required accept=".jpg, .jpeg, .png"/>
-            <div>
-            {
-        viewImageSelect && <div><img  src={viewImageSelect} alt="Img Select" height="200px"/></div>
-        
-      }
-            </div>
-            <div class="invalid-feedback">
+              <label>Image {'(jpg, jpeg, png)'}:</label>
+              <input onChange={(e) => handleChange(e)} id="files" name="image" type="file" className="form-control" required accept=".jpg, .jpeg, .png" />
+              <div>
+                {
+                  viewImageSelect ? <div><img src={viewImageSelect} alt="Img Select" height="200px" /></div> : <div>Hola img</div>
 
-              Example invalid form file feedback
+                }
+              </div>
+              <div className="invalid-feedback">
+
+                Example invalid form file feedback
+              </div>
             </div>
           </div>
+
+          <div className={`${styles.text} mb-3`}>
+
+            <label htmlFor="validationTextarea" className="form-label">Summary</label>
+            <textarea onChange={(e) => handleChange(e)} name="summary" rows="5" cols="50" placeholder="Wtite a short summary:" value={input.summary} className="form-control" id="validationTextarea" required></textarea>
+            <div className="invalid-feedback">
+              {error.summary && (
+                <div className={styles.danger}>{error.summary}</div>
+              )}
             </div>
 
-          <div class={`${styles.text} mb-3`}>
-
-            <label htmlFor="validationTextarea" class="form-label">Summary</label>
-            <textarea onChange={(e) => handleChange(e)} name="summary" rows="5" cols="50"  placeholder="Wtite a short summary:" value={input.summary} class="form-control" id="validationTextarea" required></textarea>
-            <div class="invalid-feedback">
-            { error.summary && (
-                          <div className={styles.danger}>{error.summary}</div>
-                  )}
-            </div>
-
-            <label htmlFor="validationTextarea" class="form-label">Instructions</label>
-            <textarea onChange={(e) => handleChange(e)} name="steps" rows="5" cols="50"  placeholder="Step by step" value={input.steps} class="form-control" id="validationTextarea" required></textarea>
-            <div class="invalid-feedback">
-            { error.steps && (
-                          <div className={styles.danger}>{error.steps}</div>
-                  )}
+            <label htmlFor="validationTextarea" className="form-label">Instructions</label>
+            <textarea onChange={(e) => handleChange(e)} name="steps" rows="5" cols="50" placeholder="Step by step" value={input.steps} className="form-control" id="validationTextarea" required></textarea>
+            <div className="invalid-feedback">
+              {error.steps && (
+                <div className={styles.danger}>{error.steps}</div>
+              )}
             </div>
 
           </div>
@@ -196,8 +194,8 @@ export default function AddRecipe() {
         </div>
 
 
-        <div class="mb-3">
-          <button class="btn btn-primary" type="submit" disabled={error.switchS} >Submit form</button>
+        <div className="mb-3">
+          <button className="btn btn-primary" type="submit" disabled={error.switchS} >Submit form</button>
         </div>
       </form>
     </div>
