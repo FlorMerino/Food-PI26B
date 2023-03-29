0907a9bebfd5d2ccd4914d4f203f2f6ebc3c5165
FlorMerino <florenciamerino012@gmail.com>
2023-03-29 18:29:58 -0300

    solucion process undef BTN

--- Client/src/components/ArrowUp/BTNArrowUp.jsx
+++ Client/src/components/ArrowUp/BTNArrowUp.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import {IoIosArrowDropupCircle} from 'react-icons/io';
 import styles from '../ArrowUp/BTNArrowUp.module.css';
 
 export default function ArrowUpBTN() {
- 
-
+  
+  
   window.onscroll= function (){
     let buttonUp= document.getElementById("button-up");
     let scroll= document.documentElement.scrollTop;
-
-    if( scroll>950){
-      buttonUp.style.transform ="scale(1)";
-
-    }else if (scroll <950){
-      buttonUp.style.transform ="scale(0)";
+    let path= document.URL
+  
+    if(path === 'http://localhost:3000/' || path === 'http://localhost:3000/#firstRecipe'){
+      if( scroll>950){
+        
+         buttonUp.style.transform ="scale(1)";
+  
+      }else if (scroll <950){
+       
+        buttonUp.style.transform ="scale(0)";
+      }
     }
+    
   }
 
     return(
