594e811b3dfa1352d708b9a0f7400a4f7a1881f0
FlorMerino <florenciamerino012@gmail.com>
2023-04-25 21:52:54 -0300

    import axios front

--- Client/src/index.js
+++ Client/src/index.js
@@ -5,8 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+
 const { REACT_APP_API_URL } = process.env;
+
 
 
 ///para deploy
