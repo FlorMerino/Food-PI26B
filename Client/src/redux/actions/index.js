06f4d6c847c500ae30dd533af7bad0384130d990
FlorMerino <florenciamerino012@gmail.com>
2023-04-26 18:32:26 -0300

    cambio en ruta actions

--- Client/src/redux/actions/index.js
+++ Client/src/redux/actions/index.js
@@ -14,7 +14,7 @@ export const FILTER_BY_TYPE  = 'FILTER_BY_TYPE'
 
 export function fetchRecipes() {
     return function(dispatch) {
-        axios.get('api/recipes')
+        axios.get('/api/recipes')
         .then((recipes) => {
             dispatch({
                 type: FETCH_RECIPES,
